07718ee47e4ba70c21d9de33c3efc72b4e0135b5
RafaelCTobar <rafaelctobar569@gmail.com>
2024-07-19 15:59:21 -0500

    feat add stats amount

--- front-end/src/components/Employee/BodyDashboard.tsx
+++ front-end/src/components/Employee/BodyDashboard.tsx
@@ -12,6 +12,7 @@ import OrderStats from "../Stats/OrdersStats";
 function BodyDashboard() {
     const {rut} = usePlateRUT();
     const {setOrders, orders} = useOrders();
+    const [totalIncome, setTotalIncome] = useState(0);
 
     const [currentCar, setCurrentCar] = useState({
         plate_number:"",
@@ -36,8 +37,19 @@ function BodyDashboard() {
             if(orders.error) return toast.error("Hubo un error");
             setOrders(orders.success.orders)         
         };
+
         getUser();
+        
+        const totalIncome = orders
+        .map(order => parseFloat(order.price).toFixed(2))  
+        .map(price => parseFloat(price))  
+        .reduce((acc, sum) => acc + sum, 0);   
+
+        const roundedTotalIncome = parseFloat(totalIncome.toFixed(2));
+        
+        setTotalIncome(roundedTotalIncome);
     }, [])
+
     return (
         <section className="p-6 sm:ml-64">
             <div className="grid grid-cols-3 gap-8 mb-4 text-black">
@@ -50,7 +62,7 @@ function BodyDashboard() {
                 </div>
                 <div className="flex  p-8 gap-3 flex-col items-start justify-center h-40 rounded bg-white">
                     <p className="text-3xl text-black font-medium">
-                        2
+                        1
                     </p>
                     <span className="block text-sm text-gray-800">Vehículos pendientes</span>
                     <a href="" className="text-red-500 font-semibold">Ver</a>
@@ -88,14 +100,14 @@ function BodyDashboard() {
             <div className="grid grid-cols-2 gap-8 mb-4">
                 <div className="flex  p-8 gap-3 flex-col items-start justify-center h-40 rounded bg-white">
                     <p className="text-3xl text-black font-medium">
-                        $170.00
+                        ${totalIncome}
                     </p>
                     <span className="block text-sm text-gray-800">Ingresos día</span>
                     <a href="" className="text-red-500 font-semibold">Ver</a>
                 </div>
                 <div className="flex  p-8 gap-3 flex-col items-start justify-center h-40 rounded bg-white">
                     <p className="text-3xl text-black font-medium">
-                        $200.00
+                        ${totalIncome}
                     </p>
                     <span className="block text-sm text-gray-800">Ingresos mes</span>
                     <a href="" className="text-red-500 font-semibold">Ver</a>
@@ -105,9 +117,9 @@ function BodyDashboard() {
                 <div className="col-span-2 flex flex-col items-start justify-center h-80  rounded bg-white w-full">
                     <div className="px-8 mb-4">
                         <h3 className="font-normal text-xl">Balance mensual</h3>
-                        <p className="text-2xl font-bold ">$2000 USD <span className="font-semibold text-green-500 text-base">+$5%</span></p>
+                        <p className="text-2xl font-bold ">${totalIncome} USD <span className="font-semibold text-green-500 text-base">+$5%</span></p>
                     </div>
-                    <BalanceStats/>
+                    <BalanceStats orders={orders}/>
                 </div>
                 <div className="p-8 rounded bg-white h-full">
                     <h3 className="font-bold text-2xl">Ventas mes</h3>
